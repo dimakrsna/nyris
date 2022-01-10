82cd25ef74d2a1e032b2f506345324e201061140
Dima <dimakrsna@gmail.com>
2022-01-10 20:30:01 +0200

    UI responsiveness for a laptop

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
 import { MainLayout } from "../components/Layouts/MainLayout"
 import { HomeSearch } from '../components/UI/organisms/Home/HomeSearch'
 import { HomeFooter } from "../components/UI/organisms/Home/HomeFooter"
+import styles from './Home.module.scss'
 
 export const HomePage = () => {
   return (
     <MainLayout>
-      <HomeSearch />
-      <HomeFooter />
+      <div className={styles.homeWrapper}>
+        <HomeSearch />
+        <HomeFooter />
+      </div>
     </MainLayout>
   )
 }
